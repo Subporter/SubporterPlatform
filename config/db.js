aef5a575f35e34815990a27d5588c2122ed8b314
nielsbril <niels.bril@student.howest.be>
2016-12-20 19:00:52 +0100

    identation changes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose"),
-	config = require("./subporter.config");
+    config = require("./subporter.config");
 
 let gracefulShutdown,
-	db = config.db_dev;
+    db = config.db_dev;
 
 if (process.env.NODE_ENV === 'production') {
-	db = config.db_prod;
+    db = config.db_prod;
 }
 
 mongoose.Promise = global.Promise;
@@ -13,38 +13,38 @@ mongoose.connect(db);
 
 /* Connection events */
 mongoose.connection.on("connected", () => {
-	console.log("Mongoose connected to " + db);
+    console.log("Mongoose connected to " + db);
 });
 
 mongoose.connection.on("error", (err) => {
-	console.log("Mongoose connection error: " + err);
+    console.log("Mongoose connection error: " + err);
 });
 
 mongoose.connection.on("disconnected", () => {
-	console.log("Mongoose disconnected");
+    console.log("Mongoose disconnected");
 });
 
 /* Restart events */
 /* On restart/termination */
 gracefulShutdown = (msg, callback) => {
-	mongoose.connection.close(() => {
-		console.log("Mongoose disconnected through " + msg);
-		callback();
-	});
+    mongoose.connection.close(() => {
+        console.log("Mongoose disconnected through " + msg);
+        callback();
+    });
 };
 
 /* Nodemon restart */
 process.once("SIGUSR2", () => {
-	gracefulShutdown("Nodemon restart", () => {
-		process.kill(process.pid, "SIGUSR2");
-	});
+    gracefulShutdown("Nodemon restart", () => {
+        process.kill(process.pid, "SIGUSR2");
+    });
 });
 
 /* Termination */
-process.on("SIGINT", function () {
-	gracefulShutdown("App termination", () => {
-		process.exit(0);
-	});
+process.on("SIGINT", function() {
+    gracefulShutdown("App termination", () => {
+        process.exit(0);
+    });
 });
 
 /* Schemas and models */
--- config/logger.js
+++ config/logger.js
@@ -1,24 +1,24 @@
 const winston = require("winston"),
-	winstonRotator = require("winston-daily-rotate-file");
+    winstonRotator = require("winston-daily-rotate-file");
 
 const consoleConfig = [
-	new winston.transports.Console({
-		'colorize': true
-	})
+    new winston.transports.Console({
+        'colorize': true
+    })
 ];
 
 const createLogger = new winston.Logger({
-	'transports': consoleConfig
+    'transports': consoleConfig
 });
 
 const errorLogger = createLogger;
 errorLogger.add(winstonRotator, {
-	'name': 'error-file',
-	'level': 'error',
-	'filename': './logs/error.log',
-	'json': false,
-	'datePattern': 'yyyy-MM-dd-',
-	'prepend': true
+    'name': 'error-file',
+    'level': 'error',
+    'filename': './logs/error.log',
+    'json': false,
+    'datePattern': 'yyyy-MM-dd-',
+    'prepend': true
 });
 
 module.exports.errorLog = errorLogger;
