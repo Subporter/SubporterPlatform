6aa27e76d46d8a3b3f19b8f1d7b30b34087ecc3f
nielsbril <niels.bril@student.howest.be>
2016-12-21 15:53:22 +0100

    update model addresses

--- server/models/Addresses.js
+++ server/models/Addresses.js
@@ -2,18 +2,36 @@ const mongoose = require('mongoose'),
     mongooseHidden = require('mongoose-hidden')(),
     autoIncrement = require('mongoose-sequence');
 
-let streetRegExp = /^.{1,100}$/;
+let streetRegExp = /^[a-zA-Z]{1,100}$/;
+let cityRegExp = /^[a-zA-Z]{1,50}$/;
+let countryRegExp = /^[a-zA-Z]{1,50}$/;
 
-    let addressSchema = new mongoose.Schema({
-        street: {
-            type: String,
-            required: true,
-            match: streetRegExp
-        },
-        number: {
-            type: Number,
-            required: true,
-			min: 0,
-			max: 999999
-        }
-    });
+let addressSchema = new mongoose.Schema({
+    street: {
+        type: String,
+        required: true,
+        match: streetRegExp
+    },
+    number: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 999999
+    },
+	postal: {
+		type: Number,
+		required: true,
+		min: 0,
+		max: 999999
+	},
+	city: {
+		type: String,
+		required: true,
+		match: cityRegExp
+	},
+    country: {
+        type: String,
+        required: true,
+        match: countryRegExp
+    }
+});
