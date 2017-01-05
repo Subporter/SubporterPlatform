efde03eb0512135bd328656b9a80f9140f6814dd
Arno Vande Cappelle <arnovdc@msn.com>
2017-01-05 14:13:53 +0100

    basic setup

---
+++ server/updater/index.js
@@ -0,0 +1,15 @@
+(function (updater){
+
+    var socketio = require("socket.io");
+
+    updater.init = function(server) {
+
+        var io = socketio.listen(server);
+
+        io.sockets.on("connection", function(socket){
+            console.log("socket connection");
+        });
+
+    };
+
+})(module.exports);
