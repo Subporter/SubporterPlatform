0f02b5fcf33f0754c018d6b2b21bbd16afd996ff
Simon Gemmel <simon.gemmel@student.howest.be>
2017-01-05 20:51:01 +0100

    Minor changes

---
+++ public/app/modules/Countries.js
@@ -0,0 +1,10 @@
+"use strict";
+var Country = (function () {
+    function Country(_id, name) {
+        this._id = _id;
+        this.name = name;
+    }
+    return Country;
+}());
+exports.Country = Country;
+//# sourceMappingURL=Countries.js.map
