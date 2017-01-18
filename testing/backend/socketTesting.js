9783ef66eafb3d883d3b14952eb8b51155fcc0bc
nielsbril <niels.bril@student.howest.be>
2017-01-19 00:12:18 +0100

    testing

--- testing/backend/socketTesting.js
+++ testing/backend/socketTesting.js
@@ -1,1 +1,10 @@
 // TODO: SOCKETS TESTING
+
+const mocha = require('mocha');
+
+describe('Socket testing', () => {
+	it('should start a connection', (done) => {
+		console.log('Socket tests not yet integrated');
+		done();
+	});
+});
